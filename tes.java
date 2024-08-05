7a24e28a64c88166528725b73b26241e3205313b
Suryaputr4 <surya_yustitia_33rpl@student.smktelkom-mlg.sch.id>
2024-08-05 14:04:59 +0700

    Add files via upload

---
+++ tes.java
@@ -0,0 +1,7 @@
+public class Tes { 
+    public static void main (string[]args) {
+        String nama = "Surya";
+        String kelas = "X RPL 5";
+        System.out.print("halo, nama saya " + nama + " dari kelas ");
+    }
+    }
