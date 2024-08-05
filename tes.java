b4585b153f7e7a10f05d5ddf69c8f35a36bda9d4
Suryaputr4 <surya_yustitia_33rpl@student.smktelkom-mlg.sch.id>
2024-08-05 14:24:35 +0700

    tes.java

--- tes.java
+++ tes.java
@@ -1,5 +1,5 @@
-public class Tes { 
-    public static void main (string[]args) {
+public class Tes {
+    public static void main (String[]args) {
         String nama = "Surya";
         String kelas = "X RPL 5";
         System.out.print("halo, nama saya " + nama + " dari kelas ");
